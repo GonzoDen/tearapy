ed595e4f1a14823dc196ab905d7aada82efa753f
gonzoden <deniz.nazarova.99@gmail.com>
2021-12-05 20:28:51 +0600

    made in OOP style

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
+import tkinter as tk
 from tkinter import *
 import sqlite3
 
-root = Tk()
-root.title('Tearapy')
-root.geometry('600x400')
-root.eval('tk::PlaceWindow . center')
+class Tearapy(tk.Tk):
 
-def chat():
-	bonjour = Label(root, text = "Bonjour, " + textBox.get())
-	bonjour.pack()
+	def __init__(self, *args, **kwargs):
+		tk.Tk.__init__(self, *args, **kwargs)
 
-def analize():
-	bonjour = Label(root, text = "Bon, " + textBox.get())
-	bonjour.pack()
+		self.title('Tearapy')
+		self.geometry('600x400')
+		self.eval('tk::PlaceWindow . center')
+
+		container = tk.Frame(self)
+		container.pack(side="top", fill = "both", expand = True)
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
+
+		self.frames = {}
+		for page in (StartPage, Chat):
+			page_name = page.__name__
+			frame = page(parent = container, controller = self)
+			self.frames[page_name] = frame
+			frame.grid(row=0, column=0, sticky="nsew") #put the new frame on the top
+
+		self.show_frame("StartPage")
+
+	def show_frame(self, page_name):
+		frame = self.frames[page_name]
+		frame.tkraise()
 
 
-text = Label(root, text="Bonjour! Comment tu t'appelles?")
-text.pack()
 
-textBox = Entry(root, width=15)
-textBox.pack()
+class StartPage(tk.Frame):
 
-button = Button(root, text="Chat", command = chat)
-button.pack()
+	def __init__(self, parent, controller): #read on deacoupling the pages
+		tk.Frame.__init__(self, parent)
+		self.controller = controller
+		text = Label(self, text = "Bonbon")
+		text.pack(side="top", fill="x", pady=10)
 
-button = Button(root, text="Statistics", command = analize)
-button.pack()
+		button_chat = Button(self, text = "Start Chatting", command = lambda: controller.show_frame("Chat"))
+		button_chat.pack()
 
-root.mainloop()
+
+class Chat(tk.Frame):
+
+	def __init__(self, parent, controller): 
+		tk.Frame.__init__(self, parent)
+		self.controller = controller
+		text = Label(self, text = "Start talking to the bot! Type 'quit' to stop.")
+		text.pack(side="top", fill="x", pady=10)
+
+		#USER TALKING
+		def send():
+			u_message = Label(self, text = "You: " + message_box.get())
+			u_message.pack()
+
+		message_box = Entry(self, width=30)
+		message_box.pack()
+
+		button_send = Button(self, text = "send", command = send)
+		button_send.pack()
+
+		#TODO: MACHINE REPLY
+
+
+		
+
+
+if __name__ == "__main__":
+    root = Tearapy()
+    root.mainloop()
